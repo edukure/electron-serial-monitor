c2d647d47f915c656538523df9ff5bfc2e13258d
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-03-23 20:35:18 -0300

    move viewport from _document to _app

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,18 +1,19 @@
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import 'antd/dist/antd.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
-    <>
-      <Head>
-        <title>Serial Monitor</title>
-      </Head>
+    return (
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <title>Serial Monitor</title>
+            </Head>
 
-      <Component {...pageProps} />
-    </>
-  )
-}
+            <Component {...pageProps} />
+        </>
+    );
+};
 
-export default MyApp
+export default MyApp;
